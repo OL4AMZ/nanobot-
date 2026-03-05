202d2f4fbe1d49f9b4fc74abf687bb8a84af29c1
OL4AMZ <116546706+OL4AMZ@users.noreply.github.com>
2026-03-05 09:33:48 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
+set -e
 
-# Debug：印出環境變數是否有值
-echo ">>> GOOGLE_API_KEY=${GOOGLE_API_KEY}"
-echo ">>> TELEGRAM_BOT_TOKEN=${TELEGRAM_BOT_TOKEN}"
-
-# 替換佔位符
 sed -i "s|\${GOOGLE_API_KEY}|${GOOGLE_API_KEY}|g" /root/.nanobot/config.json
 sed -i "s|\${TELEGRAM_BOT_TOKEN}|${TELEGRAM_BOT_TOKEN}|g" /root/.nanobot/config.json
 
-# Debug：印出替換後的 config
-echo ">>> config.json after replace:"
-cat /root/.nanobot/config.json
-
-# 啟動
+echo "🐈 Starting nanobot gateway on port 18790..."
 exec nanobot gateway
