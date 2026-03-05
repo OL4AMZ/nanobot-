c5987f06150302adaac80326f5bdd76c00b29699
OL4AMZ <116546706+OL4AMZ@users.noreply.github.com>
2026-03-05 09:11:24 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # 用環境變數替換 config.json 裡的佔位符
-sed -i "s|\${GEMINI_API_KEY}|${GEMINI_API_KEY}|g" /root/.nanobot/config.json
+sed -i "s|\${GOOGLE_API_KEY}|${GOOGLE_API_KEY}|g" /root/.nanobot/config.json
 sed -i "s|\${TELEGRAM_BOT_TOKEN}|${TELEGRAM_BOT_TOKEN}|g" /root/.nanobot/config.json
 
 # 啟動 nanobot gateway
